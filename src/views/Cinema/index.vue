dc0c054ad752f1c9e0939788b97731debb9f0cd2
Yozora_cyx <915658157@qq.com>
2019-08-10 18:00:48 +0800

    首页头部，底部tabbar

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Header title="喵喵影院"></Header>
+    <TabBar></TabBar>
+  </div>
+</template>
+
+<script>
+import Header from "../../components/Header/index";
+import TabBar from "../../components/TabBar/index";
+
+export default {
+  name: "Cinema",
+  components: {
+    Header,
+    TabBar
+  },
+  props: {},
+  data() {
+    return {};
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Header title="我的喵喵"></Header>
+    <TabBar></TabBar>
+  </div>
+</template>
+
+<script>
+
+import Header from '../../components/Header/index';
+import TabBar from '../../components/TabBar/index';
+
+export default {
+  name:'Mine',
+  components:{
+    Header,
+    TabBar
+  },
+  props:{},
+  data(){
+    return {
+    }
+  },
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style lang="scss" scoped>
+</style>
